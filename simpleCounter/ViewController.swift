a655e56f8f57279f04ed8adf7048597aaf1b6579
saengle <zohv817@gmail.com>
2024-03-20 18:36:53 +0900

    Add: simpleCounter

--- simpleCounter/ViewController.swift
+++ simpleCounter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let counter = Counter()
+    @IBAction func upButtonTapped(_ sender: Any) {
+        counter.countUp()
+        self.countNumLabel.text = "\(counter.countNum)"
+    }
+    @IBAction func downButtonTapped(_ sender: Any) {
+        counter.countDown()
+        self.countNumLabel.text = "\(counter.countNum)"
+    }
+    @IBOutlet weak var countNumLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+class Counter {
+    var countNum: Int = 0
+    func countUp() {
+        countNum += 1
+    }
+    func countDown() {
+        countNum -= 1
+    }
+}
